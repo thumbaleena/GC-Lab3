4b8064c1588aa7769d1b19669cf8a2a4d6c7322c
thumbaleena <thumbaleena@outlook.com>
2016-10-26 15:04:03 -0400

    .

--- LabNumber3/Program.cs
+++ LabNumber3/Program.cs
@@ -9,7 +9,49 @@ namespace LabNumber3
     class Program
     {
         static void Main(string[] args)
+
         {
+            string YesNo = "y";
+            int Input, Square, Cube, Value;
+            Console.WriteLine("Learn your squares and cubes!");
+            Console.WriteLine();
+
+            while (YesNo == "y" || YesNo == "Y")
+            {
+                Console.WriteLine("Enter an integer: ");
+                Input = int.Parse(Console.ReadLine());
+                Value = Input;
+                Console.WriteLine();
+                Console.WriteLine("Number \t   Squared \tCubed");
+                Console.WriteLine("====================================");
+                for (Input = 1; Input<=Value; Input++)
+                {
+                    Square = Input * Input;
+                    Cube = Input * Input * Input;
+                    Console.WriteLine(Input + "\t   " + Square + "\t \t" + Cube);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Continue? (y/n)");
+                YesNo = Console.ReadLine();
+                switch (YesNo)
+                {
+                    case "n":
+                    case "N":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry, try again.");
+                        YesNo = Console.ReadLine();
+                        break;
+                }
+                while (YesNo.ToLower() != "yes"  && YesNo.ToLower() != "y" && YesNo.ToLower() != "no" && YesNo.ToLower() != "n")
+                {
+                    Console.Write("Invalid entry. Try again! ");
+                    YesNo = Console.ReadLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
